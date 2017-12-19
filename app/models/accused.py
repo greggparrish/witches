7faec9a4db218bd21397bb666641926e75f47bd7
grp224 <gregg.alb@gmail.com>
2017-12-19 17:25:23 +0100

    add accused_family mvc, family_data() to get display list of existing attr for that person

--- app/models/accused.py
+++ app/models/accused.py
@@ -1,3 +1,4 @@
+import re
 from app import db
 
 ethnicities = ("Gael", "English", "Irish", "Italian", "German", "Unknown")
@@ -11,6 +12,7 @@ class Accused(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     old_id = db.Column(db.String(128))
     trials = db.relationship('Trial', backref='accused', lazy='dynamic')
+    family = db.relationship('AccusedFamily', backref='accused', lazy='dynamic')
     first_name = db.Column(db.String(128))
     last_name = db.Column(db.String(128))
     m_first_name = db.Column(db.String(128))
@@ -39,4 +41,29 @@ class Accused(db.Model):
     def __repr__(self):
         return 'Accused: {} {}'.format(self.first_name,self.last_name)
 
+class AccusedFamily(db.Model):
+    __tablename__= 'accused_family'
+    id = db.Column(db.Integer, primary_key=True)
+    accused_id = db.Column(db.Integer, db.ForeignKey(Accused.id))
+    accused_family_ref = db.Column(db.String(128))
+    accused_ref = db.Column(db.String(128))
+    first_name = db.Column(db.String(128))
+    last_name = db.Column(db.String(128))
+    alias = db.Column(db.String(128))
+    patronymic = db.Column(db.String(128))
+    destitle = db.Column(db.String(128))
+    est_birthyear = db.Column(db.String(128))
+    age = db.Column(db.Integer)
+    age_estcareer = db.Column(db.Integer)
+    age_estchild = db.Column(db.Integer)
+    occupation = db.Column(db.String(128))
+    relationship = db.Column(db.String(128))
 
+    def family_data(self):
+        ''' Make list of family member age, occupation if exists '''
+        whitelist = ['age', 'occupation']
+        return ["{}: {}".format(re.sub('_',' ',key).capitalize(),val) for key, val in self.__dict__.items() if key in whitelist and val != None]
+
+
+    def __repr__(self):
+        return 'Family of accused: {} {}'.format(self.first_name,self.last_name)
--- app/views/accused.py
+++ app/views/accused.py
@@ -1,11 +1,12 @@
 from app import app
-from app.models.accused import Accused
+from app.models.accused import Accused, AccusedFamily
 from flask import Blueprint, render_template
 from nvd3 import pieChart
 
 accused = Blueprint('accused', __name__)
 
 @accused.route('/')
+def index():
     ap = Accused.query.order_by(Accused.last_name).limit(20)
     return render_template('accused/index.html', ap=ap)
 
@@ -13,3 +14,4 @@ accused = Blueprint('accused', __name__)
 def show(accused_id):
     ap = Accused.query.filter_by(id=accused_id).first()
     return render_template('accused/show.html', ap=ap)
+
